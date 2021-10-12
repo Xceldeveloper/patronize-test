da487e25c05da31a96a9b65baa7d0b1411a38833
Xcedeveloper <xceldeveloper@gmail.com>
2021-10-12 23:35:56 +0100

    added support for globalizing atom components

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/globalize-components',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
