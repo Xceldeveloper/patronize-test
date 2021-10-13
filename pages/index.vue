6b6f8591ffe1f12bb0a5ae60c4486f25acbdc3f9
Xcedeveloper <xceldeveloper@gmail.com>
2021-10-13 17:31:17 +0100

    testing diff components

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,67 @@
 <template>
-  <div>
+  <div class="font-dm-sans text">
     Coming soon...
+    <p-button> Logout </p-button>
+
+    <br />
+    <br />
+
+    <div class="verification-method">
+      <!-- <p-text >Select a verification method </p-text> -->
+      <p-button
+        @click="cli('BVN')"
+        class="verification-method__tab-button"
+        :class="{ 'verification-method__tab-button--active': method == 'BVN' }"
+      >
+        BVN
+      </p-button>
+
+      <p-button
+        @click="cli('ACCOUNT')"
+        class="verification-method__tab-button"
+        :class="{
+          'verification-method__tab-button--active': method == 'ACCOUNT',
+        }"
+      >
+        Personal Account Number
+      </p-button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      method: "BVN",
+    };
+  },
+  methods:{
+    cli(val){
+      console.log(val)
+      this.method = val
+    }
+  }
+};
 </script>
+<style lang="scss" scoped>
+.verification-method {
+  padding: 0px 50px;
+ 
+  &__tab-button {
+    background-color: #f5f6fa;
+    color: #000;
+    height: 41px;
+    padding: 0px 20px;
+    border: 1.8px solid #1417371a;
+    border-radius: 10px;
+
+    &--active {
+      border: 1.8px solid $Blue;
+      background-color: #dceaff;
+    }
+  }
+}
+
+
+</style>
